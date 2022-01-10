07da03052e7ad6d53cde7bd06aa492a1c9b7b000
SergeyGluschenko <64856158+SergeyGluschenko@users.noreply.github.com>
2022-01-10 20:01:11 +0300

    Lesson 4 (#4)
    * Lesson 4. Start
    
    * Lesson 4. All tasks
    
    Co-authored-by: SergeyGluschenko <s_gluschenko21@mail.ruSergeyGluschenko>

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -39,8 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authnapp'
 ]
  
+# Auth model
+AUTH_USER_MODEL = 'authnapp.ShopUser' 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
